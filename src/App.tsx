04d5a7438b0281e775cea27e7477805ddbb7e501
FShinoda <fernanda.shinoda@hotmail.com>
2024-08-16 10:04:04 -0300

    feat: initial configuration done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold text-red-500 underline text-center">
+      Hello world!
+    </h1>
+    </>
+);
+}
+export default App;
